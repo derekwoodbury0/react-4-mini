75c472c27340d98b25654c39d6daed8965f110f4
Derek <Derek@Dereks-MacBook-Pro.local>
2019-06-03 14:01:55 -0600

    Complete

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,17 +1,24 @@
 import React, { Component } from "react";
 import logo from "./../../images/dm_white_logo.png";
 import "./Nav.css";
+import { Link } from 'react-router-dom'
 
 class Nav extends Component {
   render() {
     return (
       <div className="Nav">
         <div>
-          <img src={logo} alt="dm logo" />
+          <Link to="/">
+            <img src={logo} alt="dm logo" />
+          </Link>
         </div>
         <ul>
-          <li>Home</li>
-          <li>Topics</li>
+          <li>
+            <Link to="/" >Home</Link>
+          </li>
+            <li>
+              <Link to="/topics">Topics</Link>
+              </li>
         </ul>
       </div>
     );
